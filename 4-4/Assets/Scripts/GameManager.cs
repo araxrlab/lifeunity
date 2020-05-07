71d9647440f5c7409eceeef8f12b48cc585d081e
amurorei2000 <amurorei2000@gmail.com>
2020-05-07 11:50:01 +0900

    챕터 4-4 프로젝트 소스입니다.

---
+++ 4-4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // 싱글톤 변수
+    public static GameManager gm;
+    
+    private void Awake()
+    {
+        if(gm == null)
+        {
+            gm = this;
+        }
+    }
+
+    // 게임 상태 상수
+    public enum GameState
+    {
+        Ready,
+        Run,
+        GameOver
+    }
+
+    // 현재의 게임 상태 변수
+    public GameState gState;
+
+    // 게임 상태 UI 오브젝트 변수
+    public GameObject gameLabel;
+
+    // 게임 상태 UI 텍스트 컴포넌트 변수
+    Text gameText;
+
+    // PlayerMove 클래스 변수
+    PlayerMove player;
+
+    void Start()
+    {
+        // 초기 게임 상태는 준비 상태로 설정한다.
+        gState = GameState.Ready;
+
+        // 게임 상태 UI 오브젝트에서 Text 컴포넌트를 가져온다.
+        gameText = gameLabel.GetComponent<Text>();
+
+        // 상태 텍스트의 내용을 "Ready..."로 한다.
+        gameText.text = "Ready...";
+
+        // 상태 텍스트의 색상을 주황색으로 한다.
+        gameText.color = new Color32(255, 185, 0, 255);
+
+        // 게임 준비 -> 게임 중 상태로 전환하기
+        StartCoroutine(ReadyToStart());
+
+        // 플레이어 오브젝트를 찾은 뒤, 플레이어의 PlayerMove 컴포넌트 받아오기
+        player = GameObject.Find("Player").GetComponent<PlayerMove>();
+    }
+
+    IEnumerator ReadyToStart()
+    {
+        // 2초간 대기한다.
+        yield return new WaitForSeconds(2f);
+
+        // 상태 텍스트의 내용을 "Go!"로 한다.
+        gameText.text = "Go!";
+
+        // 0.5초간 대기한다.
+        yield return new WaitForSeconds(0.5f);
+
+        // 상태 텍스트를 비활성화한다.
+        gameLabel.SetActive(false);
+
+        // 상태를 "게임 중" 상태로 변경한다.
+        gState = GameState.Run;
+    }
+
+    void Update()
+    {
+        // 만일, 플레이어의 hp가 0 이하라면...
+        if(player.hp <= 0)
+        {
+            // 상태 텍스트를 활성화한다.
+            gameLabel.SetActive(true);
+
+            // 상태 텍스트의 내용을 "Game Over"로 한다.
+            gameText.text = "Game Over";
+
+            // 상태 텍스트의 색상을 붉은색으로 한다.
+            gameText.color = new Color32(255, 0, 0, 255);
+
+            // 상태를 "게임 오버" 상태로 변경한다.
+            gState = GameState.GameOver;
+        }
+    }
+
+    
+}
---
+++ 4-4/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 이동 속도 변수
+    public float moveSpeed = 7f;
+
+    // 캐릭터 콘트롤러 변수
+    CharacterController cc;
+
+    // 중력 변수
+    float gravity = -20f;
+
+    // 수직 속력 변수
+    public float yVelocity = 0;
+
+    // 점프력 변수
+    public float jumpPower = 10f;
+
+    // 점프 상태 변수
+    public bool isJumping = false;
+
+    // 플레이어 체력 변수
+    public int hp = 20;
+
+    // 최대 체력 변수
+    int maxHp = 20;
+
+    // hp 슬라이더 변수
+    public Slider hpSlider;
+
+    // Hit 효과 오브젝트
+    public GameObject hitEffect;
+
+    void Start()
+    {
+        // 캐릭터 콘트롤러 컴포넌트 받아오기
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // 게임 상태가 "게임 중" 상태일 때에만 조작 가능하게 한다.
+        if(GameManager.gm.gState != GameManager.GameState.Run)
+        {
+            return;
+        }
+
+        // 키보드 <W>, <A>, <S>, <D> 버튼을 입력하면 캐릭터를 그 방향으로 이동시키고 싶다.
+        // 키보드 <Space> 버튼을 입력하면 캐릭터를 수직으로 점프시키고 싶다.
+
+        // 1. 사용자의 입력을 받는다.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 2. 이동 방향을 설정한다.
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = dir.normalized;
+
+        // 2-1. 메인 카메라를 기준으로 방향을 변환한다.
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        // 2-2. 만일, 점프 중이었고, 다시 바닥에 착지했다면...
+        if (isJumping && cc.collisionFlags == CollisionFlags.Below)
+        {
+            // 점프 전 상태로 초기화한다.
+            isJumping = false;
+            // 캐릭터 수직 속도를 0으로 만든다.
+            yVelocity = 0;
+        }
+
+        // 2-3. 만일, 키보드 <Space> 버튼을 입력했고, 점프를 안 한 상태라면...
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            // 캐릭터 수직 속도에 점프력을 적용하고 점프 상태로 변경한다.
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        // 2-4. 캐릭터 수직 속도에 중력 값을 적용한다.
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        // 3. 이동 속도에 맞춰 이동한다.
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+
+        // 4. 현재 플레이어 hp(%)를 hp 슬라이더의 value에 반영한다.
+        hpSlider.value = (float)hp / (float)maxHp;
+    }
+
+    // 플레이어의 피격 함수
+    public void DamageAction(int damage)
+    {
+        // 에너미의 공격력만큼 플레이어의 체력을 깎는다.
+        hp -= damage;
+
+        // 만일, 플레이어의 체력이 0보다 크면 피격 효과를 출력한다.
+        if (hp > 0)
+        {
+            // 피격 이펙트 코루틴을 시작한다.
+            StartCoroutine(PlayHitEffect());
+        }
+    }
+
+    // 피격 효과 코루틴 함수
+    IEnumerator PlayHitEffect()
+    {
+        // 1. 피격 UI를 활성화 한다.
+        hitEffect.SetActive(true);
+
+        // 2. 0.3초간 대기한다.
+        yield return new WaitForSeconds(0.3f);
+
+        // 3. 피격 UI를 비활성화 한다.
+        hitEffect.SetActive(false);
+    }
+}
