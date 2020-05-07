5659a7c440edd3baf6ccbf3a734b95911aed91f7
amurorei2000 <amurorei2000@gmail.com>
2020-05-07 11:33:43 +0900

    챕터 4-2 프로젝트 소스입니다.

---
+++ 4-2/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    // 발사 위치
+    public GameObject firePosition;
+
+    // 투척 무기 오브젝트
+    public GameObject bombFactory;
+
+    // 투척 파워
+    public float throwPower = 15f;
+
+    // 피격 이펙트 오브젝트
+    public GameObject bulletEffect;
+
+    // 피격 이펙트 파티클 시스템
+    ParticleSystem ps;
+
+    void Start()
+    {
+        // 피격 이펙트 오브젝트에서 파티클 시스템 컴포넌트 가져오기
+        ps = bulletEffect.GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        // 마우스 오른쪽 버튼 입력을 받는다.
+        if (Input.GetMouseButtonDown(1))
+        {
+            // 수류탄 오브젝트를 생성하고, 수류탄의 생성 위치를 발사 위치로 한다.
+            GameObject bomb = Instantiate(bombFactory);
+            bomb.transform.position = firePosition.transform.position;
+
+            // 수류탄 오브젝트의 Rigidbody 컴포넌트를 가져온다.
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+
+            // 카메라의 정면 방향으로 수류탄에 물리적인 힘을 가한다.
+            rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+        }
+
+        // 마우스 왼쪽 버튼을 누르면 시선이 바라보는 방향으로 총을 발사하고 싶다.
+
+        // 마우스 왼쪽 버튼 입력을 받는다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 레이를 생성하고 발사될 위치와 진행 방향을 설정한다.
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            // 레이가 부딪힌 대상의 정보를 저장할 변수를 생성한다.
+            RaycastHit hitInfo = new RaycastHit();
+
+            // 레이를 발사하고, 만일 부딪힌 물체가 있으면...
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                // 피격 이펙트의 위치를 레이가 부딪힌 지점으로 이동시킨다.
+                bulletEffect.transform.position = hitInfo.point;
+
+                // 피격 이펙트의 forward 방향을 레이가 부딪힌 지점의 법선 벡터와 일치시킨다.
+                bulletEffect.transform.forward = hitInfo.normal;
+
+                // 피격 이펙트를 플레이한다.
+                ps.Play();
+            }
+        }
+    }
+}
---
+++ 4-2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 이동 속도 변수
+    public float moveSpeed = 7f;
+
+    // 캐릭터 콘트롤러 변수
+    CharacterController cc;
+
+    // 중력 변수
+    float gravity = -20f;
+
+    // 수직 속력 변수
+    public float yVelocity = 0;
+
+    // 점프력 변수
+    public float jumpPower = 10f;
+
+    // 점프 상태 변수
+    public bool isJumping = false;
+
+    // 플레이어 체력 변수
+    public int hp = 20;
+
+    void Start()
+    {
+        // 캐릭터 콘트롤러 컴포넌트 받아오기
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // 키보드 <W>, <A>, <S>, <D> 버튼을 입력하면 캐릭터를 그 방향으로 이동시키고 싶다.
+        // 키보드 <Space> 버튼을 입력하면 캐릭터를 수직으로 점프시키고 싶다.
+
+        // 1. 사용자의 입력을 받는다.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 2. 이동 방향을 설정한다.
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = dir.normalized;
+
+        // 2-1. 메인 카메라를 기준으로 방향을 변환한다.
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        // 2-2. 만일, 점프 중이었고, 다시 바닥에 착지했다면...
+        if (isJumping && cc.collisionFlags == CollisionFlags.Below)
+        {
+            // 점프 전 상태로 초기화한다.
+            isJumping = false;
+            // 캐릭터 수직 속도를 0으로 만든다.
+            yVelocity = 0;
+        }
+
+        // 2-3. 만일, 키보드 <Space> 버튼을 입력했고, 점프를 안 한 상태라면...
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            // 캐릭터 수직 속도에 점프력을 적용하고 점프 상태로 변경한다.
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        // 2-4. 캐릭터 수직 속도에 중력 값을 적용한다.
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        // 3. 이동 속도에 맞춰 이동한다.
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+    }
+}
