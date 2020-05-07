0efc0d2d5317c7efcebf763860a0cb49c841ea28
amurorei2000 <amurorei2000@gmail.com>
2020-05-07 11:56:57 +0900

    챕터 4-6 프로젝트 소스입니다.

---
+++ 4-6/Assets/Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        // 자기 자신의 방향을 카메라의 방향과 일치시킨다.
+        transform.forward = target.forward;
+    }
+}
---
+++ 4-6/Assets/Scripts/DestroyEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEffect : MonoBehaviour
+{
+    // 제거될 시간 변수
+    public float destroyTime = 1.5f;
+
+    // 경과 시간 측정용 변수
+    float currentTime = 0;
+
+    void Update()
+    {
+        // 만일 경과 시간이 제거될 시간을 초과한다면, 자기 자신을 제거한다.
+        if(currentTime > destroyTime)
+        {
+            Destroy(gameObject);
+        }
+        // 경과 시간을 누적한다.
+        currentTime += Time.deltaTime;
+    }
+}
