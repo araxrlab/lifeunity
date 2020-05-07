125cfad961a06324a8eb0b3202e43e7209a963f1
amurorei2000 <amurorei2000@gmail.com>
2020-05-07 11:36:34 +0900

    챕터 4-3 프로젝트 소스입니다.

---
+++ 4-3/Assets/Scripts/BombAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombAction : MonoBehaviour
+{
+    // 폭발 이펙트 프리팹 변수
+    public GameObject bombEffect;
+
+    // 충돌했을 때의 처리
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 이펙트 프리팹을 생성한다.
+        GameObject eff = Instantiate(bombEffect);
+
+        // 이펙트 프리팹의 위치는 수류탄 오브젝트 자신의 위치와 동일하다.
+        eff.transform.position = transform.position;
+
+        // 자기 자신을 제거한다.
+        Destroy(gameObject);
+    }
+}
