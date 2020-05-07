1ee21f7ea97062d0a4bab4d4dd09313f00158f52
amurorei2000 <amurorei2000@gmail.com>
2020-05-07 11:55:17 +0900

    챕터 4-5 프로젝트 소스입니다.

---
+++ 4-5/Assets/Scripts/CamRotate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    // 회전 속도 변수
+    public float rotSpeed = 200f;
+
+    // 회전 값 변수
+    float mx = 0;
+    float my = 0;
+           
+    void Update()
+    {
+        // 게임 상태가 "게임 중" 상태일 때에만 조작 가능하게 한다.
+        if (GameManager.gm.gState != GameManager.GameState.Run)
+        {
+            return;
+        }
+
+        // 사용자의 마우스 입력을 받아서 물체를 회전시키고 싶다.       
+        // 1. 마우스 입력을 받는다.
+        float mouse_X = Input.GetAxis("Mouse X");
+        float mouse_Y = Input.GetAxis("Mouse Y");
+
+        // 1-1. 회전 값 변수에 마우스 입력 값만큼 미리 누적시킨다.
+        mx += mouse_X * rotSpeed * Time.deltaTime;
+        my += mouse_Y * rotSpeed * Time.deltaTime;
+
+        // 1-2. 마우스 상하 이동 회전 변수(my)의 값을 -90도 ~ 90도 사이로 제한한다.
+        my = Mathf.Clamp(my, -90f, 90f);
+
+        // 2. 마우스 입력 값을 이용하여 회전 방향을 결정한다.        
+        //Vector3 dir = new Vector3(-mouse_Y, mouse_X, 0);
+
+        // 2. 회전 방향으로 물체를 회전시킨다.
+        // r = r0 + vt
+        //transform.eulerAngles += dir * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(-my, mx, 0);
+
+        // 4. x축 회전(상하 회전) 값을 -90도 ~ 90도 사이로 제한한다.
+        //Vector3 rot = transform.eulerAngles;
+        //rot.x = Mathf.Clamp(rot.x, -90f, 90f);
+        //transform.eulerAngles = rot;
+    }
+}
---
+++ 4-5/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    // 발사 위치
+    public GameObject firePosition;
+
+    // 투척 무기 오브젝트
+    public GameObject bombFactory;
+
+    // 투척 파워
+    public float throwPower = 15f;
+
+    // 피격 이펙트 오브젝트
+    public GameObject bulletEffect;
+
+    // 피격 이펙트 파티클 시스템
+    ParticleSystem ps;
+
+    // 발사 무기 공격력
+    public int weaponPower = 5;
+
+    // 애니메이터 변수
+    Animator anim;
+
+    void Start()
+    {
+        // 피격 이펙트 오브젝트에서 파티클 시스템 컴포넌트 가져오기
+        ps = bulletEffect.GetComponent<ParticleSystem>();
+
+        // 애니메이터 컴포넌트 가져오기
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        // 게임 상태가 "게임 중" 상태일 때에만 조작 가능하게 한다.
+        if (GameManager.gm.gState != GameManager.GameState.Run)
+        {
+            return;
+        }
+
+        // 마우스 오른쪽 버튼 입력을 받는다.
+        if (Input.GetMouseButtonDown(1))
+        {
+            // 수류탄 오브젝트를 생성하고, 수류탄의 생성 위치를 발사 위치로 한다.
+            GameObject bomb = Instantiate(bombFactory);
+            bomb.transform.position = firePosition.transform.position;
+
+            // 수류탄 오브젝트의 Rigidbody 컴포넌트를 가져온다.
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+
+            // 카메라의 정면 방향으로 수류탄에 물리적인 힘을 가한다.
+            rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+        }
+
+        // 마우스 왼쪽 버튼을 누르면 시선이 바라보는 방향으로 총을 발사하고 싶다.
+
+        // 마우스 왼쪽 버튼 입력을 받는다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 만일 이동 블렌드 트리 파라미터의 값이 0이라면, 공격 애니메이션을 실시한다.
+            if (anim.GetFloat("MoveMotion") == 0)
+            {
+                anim.SetTrigger("Attack");
+            }
+
+            // 레이를 생성하고 발사될 위치와 진행 방향을 설정한다.
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            // 레이가 부딪힌 대상의 정보를 저장할 변수를 생성한다.
+            RaycastHit hitInfo = new RaycastHit();
+
+            // 레이를 발사하고, 만일 부딪힌 물체가 있으면...
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                // 만일 레이에 부딪힌 대상의 레이어가 "Enemy"라면 데미지 함수를 실행한다.
+                if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                {
+                    EnemyFSM eFSM = hitInfo.transform.GetComponent<EnemyFSM>();
+                    eFSM.HitEnemy(weaponPower);
+                }
+                // 그렇지 않다면, 레이에 부딪힌 지점에 피격 이펙트를 플레이한다.
+                else
+                {
+                    // 피격 이펙트의 위치를 레이가 부딪힌 지점으로 이동시킨다.
+                    bulletEffect.transform.position = hitInfo.point;
+
+                    // 피격 이펙트의 forward 방향을 레이가 부딪힌 지점의 법선 벡터와 일치시킨다.
+                    bulletEffect.transform.forward = hitInfo.normal;
+
+                    // 피격 이펙트를 플레이한다.
+                    ps.Play();
+                }
+            }
+        }
+    }
+}
