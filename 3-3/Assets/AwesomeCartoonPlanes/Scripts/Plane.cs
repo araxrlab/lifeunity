da13e9976552dddfbf85552878fccfe9a90137e7
f11m4at4 <f11m4at4@gmail.com>
2020-05-09 18:45:36 +0900

    챕터 3-3 프로젝트 소스 입니다

---
+++ 3-3/Assets/AwesomeCartoonPlanes/Scripts/Plane.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// PLEASE NOTE! THIS SCRIPT IS FOR DEMO PURPOSES ONLY :) //
+
+public class Plane : MonoBehaviour {
+	public GameObject prop;
+	public GameObject propBlured;
+
+	public bool engenOn;
+
+	void Update () 
+	{
+		if (engenOn) {
+			prop.SetActive (false);
+			propBlured.SetActive (true);
+			propBlured.transform.Rotate (1000 * Time.deltaTime, 0, 0);
+		} else {
+			prop.SetActive (true);
+			propBlured.SetActive (false);
+		}
+	}
+}
+
+// PLEASE NOTE! THIS SCRIPT IS FOR DEMO PURPOSES ONLY :) //
